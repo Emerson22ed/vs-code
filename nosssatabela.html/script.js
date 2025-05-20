043ec94b2a985fdc49cd46a12b72ac61165ad13f
emerson <emersonquadrado5@gmail.com>
2025-05-20 09:35:01 -0300

    trabalho

---
+++ nosssatabela.html/script.js
@@ -0,0 +1,11 @@
+let button = document.querySelector("#botao")
+button.style.background="black"
+
+button.addEventListener("mouseover", e =>{button.style.background="green";})
+
+button.addEventListener("mouseout", e =>{button.style.background="blue"});
+
+
+    function mostrarMensagem() {
+            document.getElementById("mensagem")= "JESUS TE AMA!";
+          }
